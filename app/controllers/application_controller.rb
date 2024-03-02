52c8a0f9bc1872bfb88ca393f1e1e139df254f3b
elisioWander <elisio741@hotmail.com>
2024-03-02 15:16:27 -0300

    Permitir que o usuário somente acesse uma rota se ele estiver autenticado e listar as musicas vinculadas ao usuário

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::API
   def encode_token(payload)
     JWT.encode(payload, 'secret')
   end
+
+  def decode_token
+    auth_header = request.headers['Authorization']
+    puts "Valor do cabeçalho de autorização: #{auth_header}"
+
+    if auth_header
+        token = auth_header.split(' ').last
+      begin
+        JWT.decode(token, 'secret', true, { algorithm: 'HS256' })
+      rescue JWT::DecodeError => e
+        puts "Erro ao decodificar a token: #{e.message}"
+        nil
+      end
+    end
+  end
+
+  def authorized_user
+    decoded_token = decode_token()
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def authorize
+    render json: { message: 'Você precisa estar logado' }, 
+    status: :unauthorized unless authorized_user
+  end
 end
